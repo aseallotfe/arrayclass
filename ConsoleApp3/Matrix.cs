9c086c744df2e54e63909a0bd64fcda64ca4a4b6
aseallotfe <133778455+aseallotfe@users.noreply.github.com>
2023-05-17 19:35:18 +0300

    Add files via upload

---
+++ ConsoleApp3/Matrix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Matrix
+    {
+
+        /*   public int y
+          {
+              get;set;
+          }
+          public int x
+          {
+              get; set;
+          }*/
+        private int x;
+        private int y;
+        public Matrix(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Matrix()
+        {
+            Console.WriteLine("enter the number of rows");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the number of columns");
+             y = Convert.ToInt32(Console.ReadLine());
+        }
+        public void ReadMatrix() 
+        {
+            int[,] re = new int[x, y];
+            Console.WriteLine("enter the elements");
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    re[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+        public void print()
+        {
+            Console.WriteLine("the elements is");
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.WriteLine(re[i, j]);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+using System;
+
+Matrix str = new Matrix();
+str.ReadMatrix();
+str.print();
